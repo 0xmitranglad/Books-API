3c186c116458c7245eb7576b9103d5bf592f6920
mitrang <mitrang.lad@smartsensesolutions.com>
2022-05-25 11:49:16 +0530

    simple API testing and setup

--- server.js
+++ server.js
@@ -3,49 +3,74 @@ const app = express();
 const PORT = 3000;
 
 const bodyParser = require('body-parser');
-const _ = require('underscore');
+const _ = require('underscore'); 
 
-let books = [{
-    id: 1,
-    name: 'Game of thrones',
-    author: 'George Martin',
-    language: 'English',
-    publisher: 'Voyager Books',
-    edition: 'Dance of Dragons',
-    publishYear: '2011'
-},
-{
-    id: 2,
-    name: 'Game of thrones',
-    author: 'George Martin',
-    language: 'English',
-    publisher: 'Voyager Books',
-    edition: 'Dance of Dragons',
-    publishYear: '2011'
-}];
+let books = [];
+let nextId = 1;
+// let books = [{
+//     id: 1,
+//     name: 'Game of thrones',
+//     author: 'George Martin',
+//     language: 'English',
+//     publisher: 'Voyager Books',
+//     edition: 'Dance of Dragons',
+//     publishYear: '2011'
+// },
+// {
+//     id: 2,
+//     name: 'Game of thrones',
+//     author: 'George Martin',
+//     language: 'English',
+//     publisher: 'Voyager Books',
+//     edition: 'Dance of Dragons',
+//     publishYear: '2011'
+// }];
+
+// {
+//     "name": "Game of thrones",
+//     "author": "George Martin",
+//     "language": "English",
+//     "publisher": "Voyager Books",
+//     "edition": "Dance of Dragons",
+//     "publishYear": "2011"
+// }
 
 app.use(bodyParser.json());
 
-//Create 
-app.post('/', (req, res) => {
+//Create books
+app.post('/books', (req, res) => {
 
+    let body = _.pick(
+        req.body,
+        'name',
+        'author',
+        'language',
+        'publisher',
+        'edition',
+        'publishYear'
+    );
+
+    if(!_.isString(body.name) || body.name.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    body.name = body.name.trim();
+
+    body.id = nextId++;
+    books.push(body);
+
+    res.json(body);
 });
 
 //Retrive books
 app.get('/books', (req, res) => {
-    res.json(book);
+    res.json(books);
 });
 
 //Retrive books with :id
 app.get('/books/:id', (req, res) => {
     let bookId = parseInt(req.params.id, 10);
-    let matched;
-
-    books.forEach((book) => {
-        if(bookId === book.id) {
-            matched = book;
-        }
-    });
+    let matched = _.findWhere(books, {id: bookId});
     matched ? res.json(matched) : res.status(404).send();
 });
 
